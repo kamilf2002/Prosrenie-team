238d95a3e2a1f9d64731329999c885297d0b127a
Sexy sushi <steamispp2@gmail.com>
2025-11-01 22:19:03 +0300

    Черновая менюшка
    менюшка черновая

---
+++ Prosreni/prosreni/Menu/play.gd
@@ -0,0 +1,13 @@
+extends Button
+
+
+func _on_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+		get_tree().quit()
