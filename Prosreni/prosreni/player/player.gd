1dd244eafe4e3e53fe88e742acf0772176c3f296
Sexy sushi <steamispp2@gmail.com>
2025-11-02 20:11:41 +0300

    Хз
    Хз

--- Prosreni/prosreni/player/player.gd
+++ Prosreni/prosreni/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -320.0
 var jump = 0;
 
 
@@ -15,14 +15,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	if Input.is_action_just_pressed("ui_accept"):	
+	"""if Input.is_action_just_pressed("ui_accept"):	
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			jump = 0
 		else:
 			if jump == 0:
 				velocity.y = JUMP_VELOCITY
-				jump = 1
+				jump = 1"""
 		
 
 	# Get the input direction and handle the movement/deceleration.
